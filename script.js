78aca9e23623eeb8d58982e3d60eddc5586e40cd
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-05-10 01:32:12 +0200

    start working on engine

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 const model = {
 
+    "contributionInput" : document.getElementById('contribution'),
 }
+
+
+
+
+
+
+const controller = {
+    "rates" : 0,
+    "discount" : 0,
+    "gainers" : 0,
+
+    "displayCost" : function() {
+        this.rates = document.getElementById('rates').value;
+        model.contributionInput.value = this.rates;
+    }
+}
+
+
+
+
+
 
 const viewer = {
     "eventListeners" : function() {
         const form = document.getElementById('form');
         form.addEventListener('submit', function(e) {
             e.preventDefault();
-            console.log(e);
+            controller.displayCost();
+
+
         });
     }
 }
 
-const controller = {
-
-}
 
 viewer.eventListeners();
