528b08e686ca132292ca2eccad8683cfc1296315
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-05-09 22:49:56 +0200

    change bg img

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ const model = {
 }
 
 const viewer = {
-
+    "eventListeners" : function() {
+        const form = document.getElementById('form');
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            console.log(e);
+        });
+    }
 }
 
 const controller = {
 
 }
+
+viewer.eventListeners();
