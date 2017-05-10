08ede7cc4af8216dd3d00480fa8b88ec61f76409
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-05-10 10:22:54 +0200

    add calculating formula

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const model = {
-
     "contributionInput" : document.getElementById('contribution'),
+    "ratesInput" : document.getElementById('rates'),
+    "paymentFormsInput" : document.getElementById('paymentForms'),
+    "discountInput" : document.getElementById('discount'),
+    "gainersInput" : document.getElementById('gainers')
 }
 
 
@@ -10,12 +13,38 @@ const model = {
 
 const controller = {
     "rates" : 0,
+    "paymentForms" : 0,
     "discount" : 0,
     "gainers" : 0,
+    "contribution" : 0,
+
+    "setValues" : function() {
+        this.rates = parseInt(model.ratesInput.value);
+        this.paymentForms = parseInt(model.paymentFormsInput.value);
+        this.discount = parseInt(model.discountInput.value);
+        this.gainers = parseInt(model.gainersInput.value);
+
+
+        this.countContribution();
+    },
+
+    "countContribution" : function() {
+
+        this.contribution = (this.rates + (this.rates * (this.paymentForms / 100)) + (this.rates * (this.discount / 100)) + (this.rates * (this.gainers / 100)));
+        console.log(this.contribution);
+        // (this.rates * (100 + this.discount)) * (100 + this.gainers)
+
+
+
+        this.displayCost();
+    },
 
     "displayCost" : function() {
-        this.rates = document.getElementById('rates').value;
-        model.contributionInput.value = this.rates;
+        model.contributionInput.value = this.contribution;
+        console.log(this.rates);
+        console.log(this.paymentForms);
+        console.log(this.discount);
+        console.log(this.gainers);
     }
 }
 
@@ -25,11 +54,12 @@ const controller = {
 
 
 const viewer = {
+    
     "eventListeners" : function() {
         const form = document.getElementById('form');
         form.addEventListener('submit', function(e) {
             e.preventDefault();
-            controller.displayCost();
+            controller.setValues();
 
 
         });
